8c1c6b365b1ed5f021717d520dc924343b9151d7
Lucien <lucien.glasson@gmail.com>
2026-05-12 11:57:32 +0200

    added the configurations files as well as flake and home file

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+#   home-manager = {
+#     url = "github:nix-community/home-manager/release-25.11";
+#     inputs.nixpkgs.follows = "nixpkgs";
+#   };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        /home/lucien/nixos-config/configuration.nix
+#       inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
